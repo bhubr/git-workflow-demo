57e66c6f1c9b7ed9e455a9b5e3041f3c154dddaf
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-10-26 12:37:27 +0100

    Add text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,6 +5,9 @@ function Homepage() {
     <div className="Homepage">
       <h1>Welcome to Git Workflow Demo</h1>
       <h2>This is to demonstrate Pull Requests</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, atque eaque tenetur quae a ipsa porro eveniet in aliquam nesciunt, officia cumque culpa fugiat deserunt temporibus totam vitae, minima ratione!
+      </p>
     </div>
   )
 }
