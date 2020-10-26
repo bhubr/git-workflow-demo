31c5046163ef97ac7218d426e55d656c1936fc17
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-10-26 12:33:10 +0100

    init project

---
+++ src/components/MenuList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+
+const MenuList = () => {
+  return (
+    <div>
+      {/*render a MenuItem component to each element of the props array*/}
+    </div>
+  );
+};
+
+export default MenuList;
